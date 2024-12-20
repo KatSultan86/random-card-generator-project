ceb0db53b6abfd2be85cae09edebafba9814272b
KatSultan86 <shelest233@gmail.com>
2024-12-19 20:47:23 -0500

    added code

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,37 @@
-// Add your code here
+
+const number = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+const faces = ["♥", "♦", "♠", "♣"];
+
+
+
+function randomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+
+function generateRandomCard() {
+    const numup = document.querySelector(".numup");
+    const face = document.querySelector(".face");
+    const numdown = document.querySelector(".numdown");
+
+
+    //to generate random number and assign it to the top and bottom
+    const numberGenerator = randomElement(number);
+
+
+    numup.innerText = randomElement(numberGenerator);
+    face.innerText = randomElement(faces);
+    numdown.innerText = randomElement(numberGenerator);
+
+
+
+    if (randomElement(faces) === "♥" || randomElement(faces) === "♦") {
+
+        numup.style.color = 'red';
+        face.style.color = 'red';
+        numdown.style.color = 'red';
+    }
+
+
+
+}
